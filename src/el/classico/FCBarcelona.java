a22eb0bb9ce2f7ea47994be1d765d8a6bc3b1dad
Petteri Sylvänne <petteri.sylvanne@metropolia.fi>
2015-09-28 13:50:45 +0300

    Tapahtumat: Tapahtuma Maali

---
+++ src/el/classico/FCBarcelona.java
@@ -0,0 +1,50 @@
+package el.classico;        //Petteri Sylvänne
+
+public class FCBarcelona {
+    
+    String maalivahti;
+    String puolustaja;
+    String keskikentta;
+    String hyokkaaja;
+    
+    public FCBarcelona(){}
+    
+    public FCBarcelona(String mv, String puo, String kk, String hyo){
+        this.maalivahti = mv;
+        this.puolustaja = puo;
+        this.keskikentta = kk;
+        this.hyokkaaja = hyo;
+    }
+    
+    public void setMaalivahti(String maalivahti){
+        this.maalivahti = maalivahti;
+    }
+    
+    public String getMaalivahti(){
+        return maalivahti;
+    }
+    
+    public void setPuolustaja(String puolustaja){
+        this.puolustaja = puolustaja;
+    }
+    
+    public String getPuolustaja(){
+        return puolustaja;
+    }
+    
+    public void setKeskikentta(String keskikentta){
+        this.keskikentta = keskikentta;
+    }
+    
+    public String getKeskikentta(){
+        return keskikentta;
+    }
+    
+    public void setHyökkääjä(String hyokkaaja){
+        this.hyokkaaja = hyokkaaja;
+    }
+    
+    public String getHyökkääjä(){
+        return hyokkaaja;
+    }
+}
